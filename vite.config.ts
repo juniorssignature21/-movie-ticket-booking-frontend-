8e8c1f49b9ab7eba847ce9aeb7d73a29bbe0cfc7
coded <codesignature19@gmail.com>
2026-08-09 23:55:34 +0100

    fixed build error

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,12 @@ export default defineConfig({
     allowedHosts: [
       '*'
     ]
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: [
+      '*'
+    ]
   }
 })

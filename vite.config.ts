cb165ba1f9b7b5d99cdca687f01171b6ee400e20
coded <codesignature19@gmail.com>
2026-08-11 11:29:21 +0100

    edited vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      '*'
-    ]
+    allowedHosts: true
   },
   preview: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 5173,
-    allowedHosts: [
-      '*'
-    ]
+    allowedHosts: true
   }
 })
